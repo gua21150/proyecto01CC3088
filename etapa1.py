e9c92081ede90b00d38cbbbc89e4d04b2354c246
gua21150 <gua21150@uvg.edu.gt>
2022-08-20 18:52:17 -0600

    Correccion de errores

--- etapa1.py
+++ etapa1.py
@@ -4,15 +4,20 @@ from funciones import *
 # main
 # variables de configuracion
 dataset_dir = 'datasets'
+system_op = sys()
+data_path = ""
 # credenciales de la base de datos
 host, dbname, user, password = credenciales()
 # configuracion del ambiente y creacion del dataframe
 csv_files = csv_files()
 if len(csv_files) == 0:  # ya existe la carpeta
-    data_path = os.getcwd() + '/' + dataset_dir
+    if system_op == 1:  # windows
+        data_path = os.getcwd() + '\\' + dataset_dir  # windows es barras invertidas
+    elif system_op == 2:  # apple
+        data_path = os.getcwd() + '/' + dataset_dir  # ios es barra normal y una sola
     csv_files = recuperar_csv_files(data_path)
-
-configure_dataset_directory(csv_files, dataset_dir)
+else:
+    configure_dataset_directory(csv_files, dataset_dir, system_op)
 df = create_df(dataset_dir, csv_files)
 
 for k in csv_files:
@@ -21,4 +26,5 @@ for k in csv_files:
     col_str, dataframe.columns = clean_colname(dataframe)  # limpiar el nombre de las columnas, sera el atributo
     # ingresar los datos a la base de datos
     upload_to_db(host, dbname, user, password, tbl_name, col_str, k, dataframe, dataframe.columns)
-print('Todas las tablas de datos han sido importadas correctamente')
+
+print("Todas las tablas de datos han sido importadas correctamente")
--- funciones.py
+++ funciones.py
@@ -2,23 +2,26 @@ import os
 import pandas as pd
 import psycopg2
 
-""" encontrar los archivos csv dentro de la carpeta y preparar la data """
+
+# encontrar los archivos csv dentro de la carpeta y preparar la data
 def csv_files():
-    csv_files = []
+    csv_name_files = []
     for file in os.listdir(os.getcwd()):  # esto es una lista de archivos
         if file.endswith('.csv'):
-            csv_files.append(file)
-    return csv_files
+            csv_name_files.append(file)
+    return csv_name_files
+
 
 def recuperar_csv_files(path):
-    csv_files = []
+    csv_name_files = []
     for file in os.listdir(path):  # esto es una lista de archivos
         if file.endswith('.csv'):
-            csv_files.append(file)
-    return csv_files
+            csv_name_files.append(file)
+    return csv_name_files
 
-""" crea y mueve los archivos csv a un solo directorio """
-def configure_dataset_directory(csv_files, dataset_dir):
+
+# crea y mueve los archivos csv a un solo directorio """
+def configure_dataset_directory(csv_name_files, dataset_dir, op_sys):
     # creacion de la carpeta
     try:
         mkdir = 'mkdir {0}'.format(dataset_dir)
@@ -26,17 +29,17 @@ def configure_dataset_directory(csv_files, dataset_dir):
         print(mkdir)
     except:
         pass
-    print("Su equipo es sistema operativo: \n1. Windows \n2. iOS")
-    sys = int(input("Ingrese el tipo de sistema de su equipo "))
     # mover los cvs files a un nuevo directorio
-    for csv in csv_files:
-        if sys == 1:
+    mv_file = ""
+    for csv in csv_name_files:
+        if op_sys == 1:
             mv_file = "move {0} {1}".format(csv, dataset_dir)  # para windows es move
-        elif sys == 2:
+        elif op_sys == 2:
             mv_file = "mv {0} {1}".format(csv, dataset_dir)  # para ios es mv
         os.system(mv_file)
 
-"""creacion de pandas df de un archivo csv"""
+
+# creacion de pandas df de un archivo csv"""
 def create_df(dataset_directory, files):
     data_path = os.getcwd() + '/' + dataset_directory + '/'  # obtiene el path actual y lo que esta en el dataset
     df = {}  # diccionario
@@ -47,8 +50,8 @@ def create_df(dataset_directory, files):
             df[file] = pd.read_csv(data_path + file, encoding="ISO-8859-1")
     return df  # se retorna el directorio
 
-""" Limpieza de los nombres de tablas y columnas """
 
+# Limpieza de los nombres de tablas y columnas """
 # diccionario con los tipos de datos para las tablas en la base de datos
 replacements = {
     'object': 'varchar',
@@ -58,26 +61,29 @@ replacements = {
     'timedelta64[ns]': 'varchar'
 }
 
-""" Limpieza del nombre del archivo """
+
+# Limpieza del nombre del archivo """
 def clean_tbl_name(filename):
-    clean_tbl_name = filename.lower().replace(" ", "_").replace("?", "")\
-            .replace("-", "_").replace(r"/", "_").replace("\\", "_").replace("%", "")\
-            .replace(")", "").replace(r"(", "").replace("$", "")
+    clean_table_name = filename.lower().replace(" ", "_").replace("?", "") \
+        .replace("-", "_").replace(r"/", "_").replace("\\", "_").replace("%", "") \
+        .replace(")", "").replace(r"(", "").replace("$", "")
     # quitar la extension .csv
-    return '{0}'.format(clean_tbl_name.split('.')[0])  # nombre de la tabla
+    return '{0}'.format(clean_table_name.split('.')[0])  # nombre de la tabla
 
-"""Cambiar los nombres de las columnas a un formato mas limpio"""
+
+# Cambiar los nombres de las columnas a un formato mas limpio"""
 def clean_colname(data_frame):
-    data_frame.columns = [x.lower().replace(" ", "_").replace("?", "")\
-                        .replace("-", "_").replace(r"/", "_").replace("\\", "_").replace("%", "")\
-                        .replace(")", "").replace(r"(", "").replace("$", "") for x in data_frame.columns]
-
+    data_frame.columns = [x.lower().replace(" ", "_").replace("?", "").replace("-", "_") \
+                          .replace(r"/", "_").replace("\\", "_")\
+                          .replace("%", "").replace(")", "").replace(r"(", "").replace("$", "") \
+                          for x in data_frame.columns]
     # table schema
     col_str = ", ".join(
         "{} {}".format(n, d) for (n, d) in zip(data_frame.columns, data_frame.dtypes.replace(replacements)))
     return col_str, data_frame.columns
 
-""" Conecta a la base de datos e ingresa los datos """
+
+# Conecta a la base de datos e ingresa los datos
 def upload_to_db(host, dbname, user, password, tab_name, col_names, file, data_frame, data_frame_columns):
     # conexion a la base de datos
     conn_str = "host=%s dbname=%s user=%s password=%s" % (host, dbname, user, password)
@@ -112,9 +118,18 @@ def upload_to_db(host, dbname, user, password, tab_name, col_names, file, data_f
     except psycopg2.OperationalError:
         print("Alguna credencial no ha sido ingresada correctamente")
 
+
 def credenciales():
     host = str(input("Ingrese el host de su base de datos: "))
     db = str(input("Ingrese el nombre de la base de datos: "))
     user = str(input("Ingrese el usuario de pgadmin de su base de datos: "))
     passw = str(input("Ingrese la contraseña de su usuario de base de datos: "))
     return host, db, user, passw
+
+
+def sys():
+    try:
+        print("Su equipo es sistema operativo: \n1. Windows \n2. iOS")
+        return int(input("Ingrese el tipo de sistema de su equipo "))
+    except:
+        print("Ingrese un numero entero valido.\nVuelva a iniciar el programa")
